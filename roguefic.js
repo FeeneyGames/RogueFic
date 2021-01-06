f441a9b46a0ec923c96d7eaaf7b6682e6153a288
FeeneyGames <patrickfeeneygames@gmail.com>
2021-01-06 14:44:35 -0500

    Test Event

--- roguefic.js
+++ roguefic.js
@@ -3,3 +3,16 @@ function load_room(id)
     $("#room-text").load("rooms.html #" + id + " .text");
     $("#room-list").load("rooms.html #" + id + " .list");
 }
+
+function load_event(id)
+{
+    $("#event-text").load("events.html #" + id)
+    $("#room-text").css("display", 'none');
+    $("#event").css("display", '');
+}
+
+function close_event()
+{
+    $("#room-text").css("display", '');
+    $("#event").css("display", 'none');
+}
